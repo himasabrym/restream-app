e01d14936fd689fcbd1282bce4d97f2caec8d4a7
himasabrym <himasabry1@gmail.com>
2026-06-17 01:16:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const channels = {
   },
 
   ch2: {
-    input: "https://cdn.karwan.tv/biner-zaxo/index.m3u8",
+    input: "https://prod-fastly-eu-west-1.video.pscp.tv/Transcoding/v1/hls/0-t8lQTyTFQE35KdUqr0pkm-Jd-V_zyyQtTg5Dxp6TD1ov8fjo93sFLZu76IREPYe5VqWPMBoDKnTfq7oazOUQ/non_transcode/eu-west-1/periscope-replay-direct-prod-eu-west-1-public/master_dynamic_highlatency.m3u8?type=live",
     output: "rtmp://rtmp.livepeer.com/live/83e8-8n2p-hybl-3e60"
   },
 
@@ -33,7 +33,7 @@ const channels = {
   },
 
   ch4: {
-    input: "http://rgkkw.live/live/akheelasharaf/97430689947/744526.ts",
+    input: "http://185.160.192.14/live/206923845871/mI45UxamwN/255239.m3u8",
     output: "rtmp://rtmp.livepeer.com/live/00b9-vcqw-w695-h58y"
   },
 
